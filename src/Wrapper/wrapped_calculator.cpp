09a9bf806300b167496444173e1e88ae79046133
Simon Archipoff <simon.archipoff@aquilenet.fr>
2025-09-17 10:23:12 +0200

    small fix

--- src/Wrapper/wrapped_calculator.cpp
+++ src/Wrapper/wrapped_calculator.cpp
@@ -121,6 +121,7 @@ void wrapped_calculator::write_to_file()
                     Interface::netManager::instance().replyError("Calculation Document error",
                                                                  "QDesktopervices : could not open .docx file");
 #else
+                QFile file(path);
                 file.open(QFile::ReadOnly);
                 QFileDialog::saveFileContent(file.readAll(), QString::fromStdString(docxName));
 #endif
